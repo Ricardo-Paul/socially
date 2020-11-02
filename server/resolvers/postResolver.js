91f588885ec4ab0af7a04b93ff4da55d3deac08a
Ricardo-Paul <ricardopaul459@gmail.com>
2020-11-02 06:42:58 -0500

    local file upload

--- server/resolvers/postResolver.js
+++ server/resolvers/postResolver.js
@@ -1,16 +1,48 @@
+const fs = require('fs');
+const path = require('path');
 
 const Query = {
     postname: () => 'get postname'
 }
 
-// any file sent is a promise that returns
+// any file sent is a promise that resolves an object:
 // filename, mimetype, encoding, createReadStream
+
+// createReadStream can be used to pipe the file
+// to a local location or a cloud storage provider (S3, cloudinary)
+
+// stream.pipe() takes a readable stream,
+// connect it to a writable stream
 
 const Mutation = {
     createPost: async (_, { input: { image } }, {User, Post}) => {
-        const { createReadStream } = await image;
         console.log(image)
-        return "the string"
+
+        const { createReadStream, filename, mimetype, encoding } = await image;
+
+        console.log(path.join('dirname:', __dirname, '../public/uploads'));
+        const stream = createReadStream();
+        const uploadsPath = path.join(__dirname, '../public/');
+
+         new Promise((resolve, reject) => {
+            stream.pipe(fs.createWriteStream(uploadsPath))
+            .on('finish', () => {
+                console.log(
+                    {
+                        message: 'Sucess',
+                        location: uploadsPath,
+                        filename, mimetype, encoding
+                    }
+                );
+                resolve({
+                    message: 'Sucess',
+                    location: uploadsPath,
+                    filename, mimetype, encoding
+                })
+            })
+        })
+
+        return "sor"
     }
 }
 
