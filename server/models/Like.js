b8389c605d7cf9b9f3615dc894f51f7ae8bf13d2
Ricardo-Paul <ricardopaul459@gmail.com>
2020-11-06 11:16:39 -0500

    like

---
+++ server/models/Like.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const likeSchma = new Schema({
+    post:{
+        type: Schema.Types.ObjectId,
+        ref: "Post"
+    },
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{
+    timestamps: true,
+    "versionKey": "_vKey"
+})
+
+export default mongoose.model('Like', likeSchma);
