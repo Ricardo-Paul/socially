4796a79cdb545a4de41bf69b5598221d92f45a74
Ricardo-Paul <ricardopaul459@gmail.com>
2020-11-16 10:36:37 -0500

    app root component

---
+++ client/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const App = () => {
+
+    return(
+        <div> App root component </div>
+    );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+// That's the very root of the app
+// 
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+render(<App /> ,document.getElementById('root'));
