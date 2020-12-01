3e55fc2a26378cd43a88545747388914059da54c
Ricardo-Paul <ricardopaul459@gmail.com>
2020-12-01 16:54:22 -0500

    display notification count

--- client/src/components/App/AppHeader/Notification.js
+++ client/src/components/App/AppHeader/Notification.js
@@ -2,13 +2,24 @@ import { Badge, IconButton } from "@material-ui/core";
 import React from "react";
 import NotificationIcon from "@material-ui/icons/Notifications";
 import headerStyles from "./headerStyles";
+import { useStore } from "../../../store";
 
 const Notification = () => {
   const classes = headerStyles();
+  const [{auth}] = useStore();
+
+  const [notifications, setNotifications] = React.useState('')
+
+    React.useEffect(() => {
+      if(auth.user != null){
+        setNotifications(auth.user.notifications.length);
+      }
+    }, [auth]);
+
 
   return (
     <IconButton color="inherit">
-      <Badge badgeContent={10} color="secondary">
+      <Badge badgeContent={notifications} color="secondary">
         <NotificationIcon fontSize="small" />
       </Badge>
     </IconButton>
