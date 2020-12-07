354b608294776b90336d4da5472110b5c582fb07
Ricardo-Paul <ricardopaul459@gmail.com>
2020-12-07 10:26:42 -0500

    show comment notification

--- client/src/components/App/Notification.js
+++ client/src/components/App/Notification.js
@@ -5,6 +5,7 @@ import {
     ListItemText,
     Avatar
 } from '@material-ui/core';
+import { render } from 'react-dom';
 
 const Notification = ({ notification }) => {
     const avatar = "https://material-ui.com/static/images/avatar/2.jpg"
@@ -14,6 +15,14 @@ const Notification = ({ notification }) => {
 
     let senderName = notification.sender ? notification.sender.fullName : null;
 
+    const showPostImage = () => {
+        return(
+            <div style={{width:45, height:45, marginLeft:5}}>
+            <img src={notification.like.post.image} style={{width:"100%", height:"100%", objectFit:"cover"}} />
+            </div> 
+        )
+    }
+
     return(
         <ListItem button>
             <ListItemAvatar>
@@ -21,11 +30,17 @@ const Notification = ({ notification }) => {
             </ListItemAvatar>
             { notification.like && 
             <>
-            <ListItemText  secondary={`${senderName} likes your post`} />
-            <div style={{width:45, height:45, marginLeft:5}}>
-             <img src={notification.like.post.image} style={{width:"100%", height:"100%", objectFit:"cover"}} />
-            </div> 
+                <ListItemText  secondary={`${senderName} likes your post`} />
+                {showPostImage}
             </>
+            }
+
+            {
+                notification.comment && 
+                <>
+                <ListItemText secondary={`${senderName} commented your post`} />
+                {showPostImage}
+                </>
             }
         </ListItem>
     )
