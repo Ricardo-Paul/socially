55b1a3056d11add8ec6818d0c39d65edce8304f4
Ricardo-Paul <ricardopaul459@gmail.com>
2020-12-10 20:04:56 -0500

    create a message

--- server/resolvers/messageResolver.js
+++ server/resolvers/messageResolver.js
@@ -4,7 +4,15 @@ const Query = {
 
 const Mutation = {
     createMessage: async (root, { input: { message, sender, receiver } }, {Message, User}) => {
+        const newMessage = await new Message({
+            sender: sender,
+            receiver: receiver,
+            message: message
+        }).save();
 
+        // push sender to receiver messages collection
+        // push receiver to sender messages collection
+        
     },
     deleteMessage: async (root, { input: { messageId } }, { Message, User }) => {
 
