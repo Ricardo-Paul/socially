529a1d08b493ba35c37c5162f4e79571c660b962
Ricardo-Paul <ricardopaul459@gmail.com>
2020-12-15 10:16:34 -0500

    get suggested users

--- client/src/components/peopleSuggestions.js
+++ client/src/components/peopleSuggestions.js
@@ -1,8 +1,32 @@
-import { Card, Divider,CardContent } from "@material-ui/core";
 import React from "react";
+import { Card, Divider,CardContent } from "@material-ui/core";
+import { SUGGEST_PEOPLE } from "../graphql/user";
+import { useApolloClient } from "@apollo/client";
+import { useStore } from "../store";
 
 
 const PeopleSuggestions = () => {
+    const client = useApolloClient();
+    const [{auth}] = useStore();
+
+    React.useEffect(() => {
+        const getSuggestedPeople = async () => {
+            try{
+                const result = await client.query({
+                    query: SUGGEST_PEOPLE,
+                    variables: {
+                        userId: auth.user.id
+                    }
+                })
+                console.log('SUGGEST', result)
+            } catch(err){
+                console.log(err)
+            }
+        }
+
+        getSuggestedPeople();
+    },[auth])
+
     return(
         <Card>
         <h4 style={{paddingLeft: 10}}> Suggestions </h4>
