6c02a50dbe564b98606e4ab760b57979cd25ddf7
Ricardo-Paul <ricardopaul459@gmail.com>
2020-12-15 11:56:48 -0500

    formatted

--- client/src/components/peopleSuggestions.js
+++ client/src/components/peopleSuggestions.js
@@ -1,25 +1,32 @@
 import React from "react";
-import { Card, Divider, Box, List, ListItem, makeStyles, Typography } from "@material-ui/core";
+import {
+  Card,
+  Divider,
+  Box,
+  List,
+  ListItem,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
 import { SUGGEST_PEOPLE } from "../graphql/user";
 import { useApolloClient } from "@apollo/client";
 import { useStore } from "../store";
 
-const peopleStyles = makeStyles(theme => ({
-    item: {
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "flex-start",
-        padding: 5,
-        border: "1px solid #e6e6e6",
-        cursor: "pointer"
-    },
-    name: {
-        fontSize: "16px",
-        fontFamily: "roboto",
-        fontWeight: 500
-    }
-}))
-
+const peopleStyles = makeStyles((theme) => ({
+  item: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    padding: 5,
+    border: "1px solid #e6e6e6",
+    cursor: "pointer",
+  },
+  name: {
+    fontSize: "16px",
+    fontFamily: "roboto",
+    fontWeight: 500,
+  },
+}));
 
 const PeopleSuggestions = () => {
   const client = useApolloClient();
@@ -28,7 +35,6 @@ const PeopleSuggestions = () => {
 
   const classes = peopleStyles();
   const avatar = "https://material-ui.com/static/images/avatar/2.jpg";
-
 
   React.useEffect(() => {
     const getSuggestedPeople = async () => {
@@ -48,34 +54,30 @@ const PeopleSuggestions = () => {
     getSuggestedPeople();
   }, [auth]);
 
-    if(!people.length > 0){
-        return null
-    };
-    
+  if (!people.length > 0) {
+    return null;
+  }
 
   return (
-    <Card style={{width: "80%"}}>
+    <Card style={{ width: "80%" }}>
       <h4 style={{ paddingLeft: 10 }}> USER SUGGESTIONS </h4>
       <Divider />
-          <List style={{padding: 0}}>
-              {people.map( p => (
-                  <ListItem disableGutters className={classes.item}>
-                      <img 
-                      src={avatar} 
-                      style={{width: 80, height: 80, marginRight: 10}} />
-                      <Box
-                      display="flex"
-                      flexDirection="column"
-                      >
-                        <Typography className={classes.name} > {p.fullName} </Typography>
-                        <Typography variant="body2"> @{p.username} </Typography>
-                      </Box>
-                  </ListItem>
-              ))}
-          </List>
+      <List style={{ padding: 0 }}>
+        {people.map((p) => (
+          <ListItem disableGutters className={classes.item}>
+            <img
+              src={avatar}
+              style={{ width: 80, height: 80, marginRight: 10 }}
+            />
+            <Box display="flex" flexDirection="column">
+              <Typography className={classes.name}> {p.fullName} </Typography>
+              <Typography variant="body2"> @{p.username} </Typography>
+            </Box>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   );
-    
 };
 
 export default PeopleSuggestions;
