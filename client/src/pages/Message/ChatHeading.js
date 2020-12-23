b020fca8933c9342adb65c1c24b73bfc48270deb
Ricardo-Paul <ricardopaul459@gmail.com>
2020-12-23 11:14:38 -0500

    Chat Header

---
+++ client/src/pages/Message/ChatHeading.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Avatar, Typography, Box, makeStyles } from "@material-ui/core";
+import Search from "../../components/Search/Search";
+
+const useStyles = makeStyles(theme => ({
+    user: {
+        display: "flex",
+        width: "100%",
+        justifyContent: "flex-start",
+        textDecoration: "none",
+        alignItems: "center",
+        padding: 5,
+        color: "#ffffff",
+        backgroundColor: "#ababab"
+    }
+}))
+
+const ChatHeading = ({ chatUser }) => {
+    const classes = useStyles();
+
+    if(chatUser){
+        return(
+            <Box className={classes.user}>
+                <Avatar style={{marginRight: 15}} src={chatUser.image} />
+                <Typography> { chatUser.fullName } </Typography>
+            </Box>
+        )
+    }
+
+    if(!chatUser){
+        return(
+            <Search />
+        )
+    }
+}
+
+ChatHeading.propTypes = {
+    chatUser: PropTypes.object
+}
+
+export default ChatHeading;
--- client/src/pages/Message/MessageChat.js
+++ client/src/pages/Message/MessageChat.js
@@ -1,21 +1,31 @@
 import React from "react";
 import { Box, makeStyles } from "@material-ui/core";
+import ChatHeading from "./ChatHeading";
 
 const useStyles = makeStyles(theme => ({
     container: {
         backgroundColor: "#dedede",
         width: "100%",
         height:"90vh",
-        padding: 5
     }
-}))
+}));
+
+const avatar = "https://material-ui.com/static/images/avatar/3.jpg"
+
+const user = {
+    id: 1,
+    image: avatar,
+    fullName: "Roger Alexander",
+    lastMessageSender: true,
+    lastMessage: "Hey fine"
+}
 
 const MessageChat = () => {
     const classes = useStyles();
 
     return(
         <Box className={classes.container}>
-            MESSAGE CHAT
+            <ChatHeading chatUser={user} />
         </Box>
     )
 }
