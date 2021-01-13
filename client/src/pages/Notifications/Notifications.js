79bbdb65c3cf37b63335395004d6c1f71d18c693
Ricardo-Paul <ricardopaul459@gmail.com>
2021-01-13 13:14:10 -0500

    show notifications count

--- client/src/pages/Notifications/Notifications.js
+++ client/src/pages/Notifications/Notifications.js
@@ -3,13 +3,14 @@ import { GET_USER_NOTIFICATIONS } from "../../graphql/notification"
 import { useQuery } from "@apollo/client"
 import { useStore } from "../../store"
 import Notification from "../../components/App/Notification"
+import { Typography } from "@material-ui/core"
 
 const Notifications = () => {
     const [{auth}] = useStore();
 
     // TODO: rep skip and limit with constant VARS
     // USE FetchMore
-    const { data, loading, error, networkStatus } = useQuery(GET_USER_NOTIFICATIONS, {
+    const { data, loading, networkStatus } = useQuery(GET_USER_NOTIFICATIONS, {
         variables: {
             userId: auth.user.id,
             skip: 0,
@@ -17,7 +18,8 @@ const Notifications = () => {
         },
         notifyOnNetworkStatusChange: true
     })
-    
+
+    const count = data ? data.getUserNotifications.count : null
 
     const renderContent = () => {
         if (loading && networkStatus === 1) {
@@ -27,12 +29,7 @@ const Notifications = () => {
           if (loading && networkStatus === 3) {
             return <h4> Loading more... </h4>;
           }
-      
-          console.log(data);
-          if (error) {
-            console.log(error);
-          }
-      
+
           if (!loading && networkStatus != 1) {
             const notifications = data.getUserNotifications.notifications;
       
@@ -47,7 +44,10 @@ const Notifications = () => {
 }
 
     return(
-        renderContent()
+        <React.Fragment>
+            <Typography> Notifications {count} </Typography>
+            {renderContent()}
+        </React.Fragment>
     )
 }
 
