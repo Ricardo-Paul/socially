2fac45229082cefb4b21e5ebc7cf15ce4c46cbe6
Ricardo-Paul <ricardopaul459@gmail.com>
2021-01-15 10:39:34 -0500

    more concise algorith for infinite scrolling

--- client/src/components/InfiniteScrolling.js
+++ client/src/components/InfiniteScrolling.js
@@ -2,9 +2,6 @@ import { useEffect, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { get, uniqBy, setWith, clone } from 'lodash';
 
-/**
- * Component that adds Infinite scroll functionality to UI
- */
 const InfiniteScrolling = ({ data, dataKey, fetchMore, variables, count, children }) => {
   const handleScroll = useMemo(
     () => async () => {
@@ -12,22 +9,19 @@ const InfiniteScrolling = ({ data, dataKey, fetchMore, variables, count, childre
         alert('HIT PAGE BOTTOM')
       };
 
-    //   const windowHeight = window.innerHeight;
-    //   const scrollTop = document.documentElement.scrollTop;
-    //   const offsetHeight = document.documentElement.offsetHeight;
-    //   const scrolled = windowHeight + scrollTop > offsetHeight - offsetHeight / 3;
 
-      const scrolled = window.innerHeight + window.pageYOffset >= document.body.offsetHeight
-
-      // Stop event listener if all the data has been loaded
       if (data.length >= count) {
         window.removeEventListener('scroll', handleScroll);
         return;
       }
 
-      // Load more data if user has scrolled to bottom and if there's still data in db to display
+      // determine when the page is scrolled near the bottom (-300px)
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollPos = window.innerHeight + document.documentElement.scrollTop;
+      const scrolled = (scrollHeight - 300 >= scrollPos) / scrollHeight == 0;
+
       if (scrolled) {
-        window.removeEventListener('scroll', handleScroll);
+        // window.removeEventListener('scroll', handleScroll)
         loadMore();
       }
     },
@@ -37,7 +31,7 @@ const InfiniteScrolling = ({ data, dataKey, fetchMore, variables, count, childre
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
 
-    // return () => window.removeEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
   }, [handleScroll]);
 
   return children(data);
