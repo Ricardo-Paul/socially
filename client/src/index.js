ea2cde414dbff351d05b356f2a443b877bd9b9e9
Ricardo-Paul <ricardopaul459@gmail.com>
2021-02-03 13:45:10 -0500

    start s

--- client/src/index.js
+++ client/src/index.js
@@ -105,8 +105,8 @@ if(SELECTED_THEME === DARK_THEME || SELECTED_THEME === LIGHT_THEME ){
 }
 
 // http and websockekt links
-const apiUrl = "https://intense-mountain-10804.herokuapp.com/graphql";
-const webSocketApiUrl = "ws://intense-mountain-10804.herokuapp.com/graphql";
+const apiUrl = "https://intense-mountain-10804.herokuapp.com/graphql/";
+const webSocketApiUrl = "ws://intense-mountain-10804.herokuapp.com/graphql/";
 const client = createApolloClient(apiUrl, webSocketApiUrl);
 
 render(
