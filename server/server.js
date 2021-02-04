da66a44073fc9a24add3d5703563114798c402ed
Ricardo-Paul <ricardopaul459@gmail.com>
2021-02-04 16:23:55 -0500

    c

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ httpServer.listen({ port: process.env.PORT || 8000}, () => {
     `)
 })
 
-mongoose.connect(LOCAL_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_ATLAS, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => {
     console.log(`API Connected to the databse: ${DB_NAME}`)
 })
